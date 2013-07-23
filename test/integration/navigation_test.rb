ee77b9b080dd2f989e59e1d86063df1cbe0fe32e
Daniel Puglisi <pulleasy@gmail.com>
2013-07-23 14:55:22 +0200

    added integration test

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -4,11 +4,11 @@ class NavigationTest < ActionDispatch::IntegrationTest
 
   def test_returns_navigation
     visit "/"
-    assert has_content? "Home"
-    assert has_content? "Products"
-    assert has_content? "Kreatify"
-    assert has_content? "Ruvetia"
-    assert has_content? "About"
-    assert has_content? "Contact"
+    assert has_content?("Home")
+    assert has_content?("Products")
+    assert has_content?("Kreatify")
+    assert has_content?("Ruvetia")
+    assert has_content?("About")
+    assert has_content?("Contact")
   end
 end
