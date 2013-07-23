589a2446fe79acae5e53442cea226495f10b3453
Daniel Puglisi <pulleasy@gmail.com>
2013-07-23 14:55:14 +0200

    config and autoload

--- lib/seiten.rb
+++ lib/seiten.rb
@@ -13,4 +13,13 @@ module Seiten
   def self.config
     @config
   end
+
+  module Controllers
+    autoload :Helpers, 'seiten/controllers/helpers'
+  end
+
+  ActiveSupport.on_load(:action_controller) do
+    include Controllers::Helpers
+    helper_method :current_page
+  end
 end
