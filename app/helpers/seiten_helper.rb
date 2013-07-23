b14e12beb72580ec3e720e0ffcc4cae3ae54c0cd
Daniel Puglisi <pulleasy@gmail.com>
2013-07-23 14:50:11 +0200

    merged pages and navigation into seiten helper

--- app/helpers/seiten_helper.rb
+++ app/helpers/seiten_helper.rb
@@ -1,15 +1,23 @@
-module NavigationHelper
-  require 'ext/string'
+module SeitenHelper
+
+  def render_html
+    if params[:page]
+      filename = params[:page]
+    else
+      filename = "home"
+    end
+    render :file => File.join(Rails.root, Seiten.config[:storage_directory], filename)
+  end
 
   def seiten_navigation(parent_id=nil, deep=2)
     output ||= ""
 
     if deep > 0
-      Page.find_by_parent_id(parent_id).each do |page|
-        status = page.active?(@current_page) ? "active" : "inactive"
+      Seiten::Page.find_by_parent_id(parent_id).each do |page|
+        status = page.active?(current_page) ? "active" : "inactive"
         output += "<li class='#{status}'>#{link_to(page.title, page.slug)}"
         unless page.children.blank?
-          output += navigation(page.id, deep-1)
+          output += seiten_navigation(page.id, deep-1)
         end
         output += "</li>"
       end
@@ -19,7 +27,7 @@ module NavigationHelper
 
   def seiten_breadcrumb
     output = content_tag(:ul, class: "breadcrumb") do
-      Page.get_breadcrumb(@current_page).reverse.collect { |page|
+      Seiten::Page.get_breadcrumb(current_page).reverse.collect { |page|
         content_tag :li do
           raw "> #{link_to(page.title, page.slug)}"
         end
