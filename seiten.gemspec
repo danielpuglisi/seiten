b7675d50d999796518dc601f51eb4f5078606f5e
Daniel Puglisi <pulleasy@gmail.com>
2013-08-28 11:37:41 +0200

    add rails ~> 3.2 as a development dependency

--- seiten.gemspec
+++ seiten.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency "rails", ">= 3.2.0"
 
+  s.add_development_dependency "rails", "~> 3.2"
   s.add_development_dependency "sqlite3", "~> 1.3.7"
   s.add_development_dependency "capybara", "~> 2.1.0"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,11 +10,6 @@ Rails.backtrace_cleaner.remove_silencers!
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
-# Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-end
-
 class Test::Unit::TestCase
   # Seiten.config[:storage_type]      = :yaml
   # Seiten.config[:storage_file]      = File.join('..', 'fixtures', 'navigation.yml')
