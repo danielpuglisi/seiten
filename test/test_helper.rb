94383f65dfe54147a0621a4635717ee14b303a6f
Daniel Puglisi <daniel@codegestalt.com>
2016-12-19 21:29:27 +0100

    cleanup test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,19 +9,3 @@ Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-class ActiveSupport::TestCase
-  # setup do
-  #   Seiten::Navigation.set_current_page_store(storage_language: :en)
-  # end
-  # Seiten.config[:storage_type]      = :yaml
-  # Seiten.config[:storage_file]      = # File.join('dummy', '', 'navigation.yml')
-  # Seiten.config[:storage_directory] = # File.join('app', 'pages')
-end
-
-class ActionDispatch::IntegrationTest
-  include Capybara::DSL
-  # setup do
-  #   Seiten::PageStore.set_current_page_store(storage_language: :en)
-  # end
-end
