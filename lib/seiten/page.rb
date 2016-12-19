c6b60190e1dd8ecc105c2da0281d02180ca94add
Daniel Puglisi <daniel@codegestalt.com>
2016-12-19 19:06:27 +0100

    refactor Seiten::Page

--- lib/seiten/page.rb
+++ lib/seiten/page.rb
@@ -1,53 +1,42 @@
 module Seiten
-
   class Page
-
-    attr_accessor :id, :parent_id, :title, :slug, :redirect, :layout, :metadata
+    attr_accessor :navigation_id, :id, :parent_id, :title, :slug, :redirect, :layout, :metadata
 
     # initialize Page object with attributes
     def initialize(options={})
-      @id         = options[:id]
-      @parent_id  = options[:parent_id]
-      @title      = options[:title]
-      @slug       = options[:slug]
-      @redirect   = options[:redirect]
-      @layout     = options[:layout]
-      @metadata   = options[:metadata].each_with_object({}){|(k,v), h| h[k.to_sym] = v} if options[:metadata]
+      @navigation_id = options[:navigation_id]
+      @id            = options[:id]
+      @parent_id     = options[:parent_id]
+      @title         = options[:title]
+      @slug          = options[:slug]
+      @redirect      = options[:redirect]
+      @layout        = options[:layout]
+      @metadata      = options[:metadata].each_with_object({}){|(k,v), h| h[k.to_sym] = v} if options[:metadata]
       @metadata ||= {}
     end
 
-    class << self
+    # class << self
 
-      def all
-        Seiten::Navigation.current.pages
-      end
+    #   # find a page by slug
+    #   def find_by_slug(slug)
+    #     slug = slug[1..-1] if slug && slug[0] == "/"
+    #     all.select { |page| page.slug == slug }.first
+    #   end
 
-      # find page by id
-      def find(id)
-        all.select { |page| page.id == id }.first
-      end
+    #   # get breadcrumb of given page (reversed)
+    #   def get_breadcrumb(page)
+    #     pages ||= []
+    #     pages << page
+    #     if page.parent
+    #       pages << get_breadcrumb(page.parent)
+    #     end
+    #     pages.flatten
+    #   end
 
-      # find all pages by parent_id
-      def find_by_parent_id(parent_id)
-        all.select { |page| page.parent_id == parent_id }
-      end
+    # end
 
-      # find a page by slug
-      def find_by_slug(slug)
-        slug = slug[1..-1] if slug && slug[0] == "/"
-        all.select { |page| page.slug == slug }.first
-      end
-
-      # get breadcrumb of given page (reversed)
-      def get_breadcrumb(page)
-        pages ||= []
-        pages << page
-        if page.parent
-          pages << get_breadcrumb(page.parent)
-        end
-        pages.flatten
-      end
-
+    def navigation
+      Seiten::Navigation.find_by(id: navigation_id)
     end
 
     # returns true if slug starts with http:// or https://
@@ -57,18 +46,17 @@ module Seiten
 
     # get parent of page
     def parent
-      Page.find(parent_id)
+      navigation.pages.find(parent_id)
     end
 
     def parent?
       parent.present?
     end
 
-    # TODO: Find a better name for this
     # get root page of current page branch
-    def branch_root
+    def root
       if self.parent?
-        self.parent.branch_root
+        self.parent.root
       else
         self
       end
@@ -76,7 +64,7 @@ module Seiten
 
     # get children of page
     def children
-      Page.all.select { |page| page.parent_id == id }
+      navigation.pages.where(parent_id: id)
     end
 
     # true if child is children of page
--- test/lib/seiten/page_test.rb
+++ test/lib/seiten/page_test.rb
@@ -2,108 +2,118 @@ require 'test_helper'
 
 class Seiten::PageTest < ActiveSupport::TestCase
 
-  def test_returns_correct_number_of_page_objects
-    pages = Seiten::Page.all
-    assert_equal 17, pages.count
+  def navigation
+    @navigation ||= Seiten::Navigation.new(name: :test, locale: :en)
   end
 
-  def test_returns_page_title
-    assert_equal "Home", Seiten::Page.find(1).title
+  def pages
+    @pages ||= navigation.pages
   end
 
-  def test_returns_empty_string_for_root_page_slug
-    assert_equal "", Seiten::Page.find(1).slug
+  setup do
+    Seiten.navigations << navigation
   end
 
-  def test_returns_page_slug
-    assert_equal "about/our-team", Seiten::Page.find(8).slug
+  teardown do
+    Seiten.navigations = []
+    navigation.pages = []
   end
 
-  def test_returns_page_layout
-    assert_equal "home", Seiten::Page.find(1).layout
+  test '.navigation' do
+    page = pages.new
+
+    assert_equal navigation, page.navigation
   end
 
-  def test_returns_external?
-    assert_equal true, Seiten::Page.find(14).external?
-    assert_equal false, Seiten::Page.find(5).external?
+  test '.external?' do
+    page = pages.new
+
+    page.slug = 'about-us'
+    assert_not page.external?
+
+    page.slug = 'http://codegestalt.com'
+    assert page.external?
   end
 
-  def test_returns_parent_page
-    page = Seiten::Page.find(4)
-    assert_equal Seiten::Page.find(3).title, page.parent.title
+  test '.parent' do
+    parent_page = pages.new(id: 5)
+    child_page  = pages.new(id: 10, parent_id: 5)
+
+    assert_equal parent_page, child_page.parent
+    assert_nil parent_page.parent
   end
 
-  def test_returns_true_if_page_has_a_parent
-    page = Seiten::Page.find(4)
-    assert_equal true, page.parent?
+  test '.parent?' do
+    parent_page = pages.new(id: 5)
+    child_page  = pages.new(id: 10, parent_id: 5)
+
+    assert child_page.parent?
+    child_page.parent_id = nil
+    assert_not parent_page.parent?
+    child_page.parent_id = 4
+    assert_not child_page.parent?
   end
 
-  def test_returns_false_if_page_has_no_parent
-    page = Seiten::Page.find(1)
-    assert_equal false, page.parent?
+  test '.root' do
+    parent_page  = pages.new(id: 5)
+    child_page_1 = pages.new(id: 10, parent_id: 5)
+    child_page_2 = pages.new(id: 15, parent_id: 10)
+
+    assert_equal parent_page, child_page_2.root
+    assert_equal parent_page, child_page_1.root
+    assert_equal parent_page, parent_page.root
   end
 
-  def test_returns_branch_root_page
-    page = Seiten::Page.find(14)
-    assert_equal Seiten::Page.find(7), page.branch_root
-    page = Seiten::Page.find(4)
-    assert_equal Seiten::Page.find(3), page.branch_root
-    page = Seiten::Page.find(1)
-    assert_equal Seiten::Page.find(1), page.branch_root
+  test '.children' do
+    parent_page  = pages.new(id: 5)
+    child_page_1 = pages.new(id: 10, parent_id: 5)
+    child_page_2 = pages.new(id: 15, parent_id: 5)
+    child_page_3 = pages.new(id: 20, parent_id: 5)
+
+    assert_equal [child_page_1, child_page_2, child_page_3], parent_page.children
   end
 
-  def test_returns_children_pages
-    page = Seiten::Page.find(3)
-    assert_equal ["Logo Design", "Web Development", "Hire us"], page.children.map(&:title)
+  test '.parent_of?' do
+    parent_page  = pages.new(id: 5)
+    child_page_1 = pages.new(id: 10, parent_id: 5)
+    child_page_2 = pages.new(id: 15, parent_id: 10)
+    sibling_page = pages.new(id: 25)
+
+    assert parent_page.parent_of?(child_page_1)
+    assert parent_page.parent_of?(child_page_2)
+    assert child_page_1.parent_of?(child_page_2)
+
+    assert_not child_page_1.parent_of?(parent_page)
+    assert_not child_page_2.parent_of?(parent_page)
+
+    assert_not sibling_page.parent_of?(parent_page)
+    assert_not parent_page.parent_of?(sibling_page)
   end
 
-  def test_returns_true_if_page_is_child_of_parent
-    child = Seiten::Page.find(4)
-    assert_equal true, Seiten::Page.find(3).parent_of?(child)
+  test '.active?' do
+    parent_page  = pages.new(id: 5)
+    child_page   = pages.new(id: 10, parent_id: 5)
+    sibling_page = pages.new(id: 15)
+
+    assert parent_page.active?(parent_page)
+    assert parent_page.active?(child_page)
+    assert_not parent_page.active?(sibling_page)
+
+    assert child_page.active?(child_page)
+    assert_not child_page.active?(parent_page)
+    assert_not child_page.active?(sibling_page)
+
+    assert_nil parent_page.active?(nil)
   end
 
-  def test_returns_true_if_page_child_is_deeply_nested
-    child = Seiten::Page.find_by_slug("about/our-team")
-    assert_equal true, Seiten::Page.find_by_slug("about").parent_of?(child)
-  end
+  # def test_returns_metadata
+  #   page = Seiten::Page.find_by_slug("products/logo-design")
+  #   assert_equal({ header_image: "logo.jpg", description: "Our logo design is awesome." }, page.metadata)
+  #   assert_equal "logo.jpg", page.metadata[:header_image]
+  # end
 
-  def test_returns_false_if_page_is_not_child_of_parent
-    wrong_child = Seiten::Page.find(1)
-    assert_equal false, Seiten::Page.find(3).parent_of?(wrong_child)
-  end
-
-  def test_returns_true_for_active_page
-    active_page = Seiten::Page.find(1)
-    assert_equal true, active_page.active?(active_page)
-  end
-
-  def test_returns_true_for_parent_of_active_page
-    active_page = Seiten::Page.find(4)
-    assert_equal true, Seiten::Page.find(3).active?(active_page)
-  end
-
-  def test_returns_false_for_not_active_page
-    active_page = Seiten::Page.find(1)
-    assert_equal false, active_page.active?(Seiten::Page.find(2))
-  end
-
-  def test_finds_page_by_slug
-    page = Seiten::Page.find_by_slug("products/logo-design")
-    assert_equal "Logo Design", page.title
-  end
-
-  def test_returns_nil_for_active_method_when_current_page_is_nil
-    assert_equal nil, Seiten::Page.find(1).active?(nil)
-  end
-
-  def test_returns_metadata
-    page = Seiten::Page.find_by_slug("products/logo-design")
-    assert_equal({ header_image: "logo.jpg", description: "Our logo design is awesome." }, page.metadata)
-    assert_equal "logo.jpg", page.metadata[:header_image]
-  end
-
-  def test_returns_empty_hash_when_metadata_are_not_set
-    page = Seiten::Page.find_by_slug("contact")
-    assert_equal({}, page.metadata)
-  end
+  # def test_returns_empty_hash_when_metadata_are_not_set
+  #   page = Seiten::Page.find_by_slug("contact")
+  #   assert_equal({}, page.metadata)
+  # end
 end
