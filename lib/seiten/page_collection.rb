df8843efd21c09ca31a25a319e1714ada0b54ae0
Daniel Puglisi <daniel@codegestalt.com>
2020-10-01 16:37:51 +0200

    Seiten::PageCollection cleanup and memoization

--- lib/seiten/page_collection.rb
+++ lib/seiten/page_collection.rb
@@ -2,42 +2,52 @@ module Seiten
   class PageCollection
     attr_accessor :navigation_id, :pages
 
-    def initialize(options={})
+    def initialize(options = {})
       @navigation_id = options[:navigation_id]
       @pages         = options[:pages] || []
     end
 
     def navigation
-      Seiten::Navigation.find_by(id: self.navigation_id)
+      Seiten::Navigation.find_by(id: navigation_id)
     end
 
-    def build(options={})
+    def build(options = {})
       Seiten::PageCollectionBuilder.call(self, options)
     end
 
     def all
-      self.pages.to_a
+      pages.to_a
     end
 
     def find(id)
-      self.pages.select { |page| page.id == id }.first
+      find_by(id: id)
     end
 
     def find_by(params)
-      where(params).first
-    end
+      @find_by ||= {}
+      return @find_by[params] if @find_by.key?(params)
 
-    def where(params)
-      self.pages.select do |page|
-        params.all? do |param|
-          page.send(param[0]) == param[1]
+      @find_by[params] = pages.find do |page|
+        params.all? do |k, v|
+          page.send(k) == v
         end
       end
     end
 
-    def new(params={})
+    def where(params)
+      @where ||= {}
+      return @where[params] if @where.key?(params)
+
+      @where[params] = pages.select do |page|
+        params.all? do |k, v|
+          page.send(k) == v
+        end
+      end
+    end
+
+    def new(params = {})
       page = Seiten::Page.new(params.merge(navigation_id: navigation_id))
-      self.pages << page
+      pages << page
       page
     end
   end
