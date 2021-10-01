19b9460418510d7ceb93b0df8c142def5488a81b
Daniel Puglisi <daniel@codegestalt.com>
2021-10-01 12:26:17 +0200

    allow overwriting :to option for seiten route helper

--- lib/seiten/routes_helper.rb
+++ lib/seiten/routes_helper.rb
@@ -1,17 +1,15 @@
 class ActionDispatch::Routing::Mapper
   def seiten(*resources)
-
     options = resources.extract_options!
-    options[:to] = "seiten/pages#show"
+    options[:to] ||= 'seiten/pages#show'
 
     resources.each do |resource|
       resource_options = options.dup
 
       resource_options[:as]     ||= resource == :application ? :seiten_page : "seiten_#{resource}_page"
       resource_options[:defaults] = resource == :application ? {} : { navigation_id: resource }
-      # resource_options[:defaults] = { navigation_id: resource }
 
-      get "(*page)", resource_options
+      get '(*page)', resource_options
     end
   end
 end
